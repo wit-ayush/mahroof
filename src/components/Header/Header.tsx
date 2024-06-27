86eec36abe3d8c7a5f0b6d2a1dcf4acd645f830f
Ayush Tripathi <ayush.tripathi.8757@gmail.com>
2024-06-27 16:40:43 +0530

    header_fixed_click

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,8 +4,8 @@ import './Header.css';
 const Header: React.FC = () => {
   return (
     <header className="Header">
-      <img src="https://seeklogo.com/images/L/logo-com-hr-logo-5636A4D2D5-seeklogo.com.png" alt="Logo" className="Logo" />
-      <h1 className="BrandName">Mahroof</h1>
+      <img src={require("../../assests/click_logo_c.png")} alt="Logo" className="Logo" />
+      <img src={require("../../assests/click_logo_letters.png")} alt="Logo" className="LogoText" />
     </header>
   );
 }
